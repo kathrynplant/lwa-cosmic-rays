914bc367a6b7345b088c76314560005d49d340f5
Kathryn Plant <plant.kathryn.a@gmail.com>
2023-07-10 20:45:47 +0000

    Committing version of preliminary_cuts.py and associated configuration used for processing the July 4 data.

--- preliminary_cuts.py
+++ preliminary_cuts.py
@@ -14,11 +14,11 @@ import yaml
 
 parser=argparse.ArgumentParser(description='Parse raw cosmic ray trigger data files and make preliminary RFI rejection cuts.')
 parser.add_argument('fname', type=str, help='File name of raw data file, not including file path (which is specified in config file)')
-parser.add_argument('config',type=str, help='Full path to configuration file')
+#TODO bring back this command line option parser.add_argument('config',type=str, help='Full path to configuration file')
 args=parser.parse_args()
 fname = args.fname
-config = args.config
-
+#TODO bring back this option config = args.config
+config='/home/ubuntu/kp/lwa-cosmic-rays/config_preliminary_cuts.yml'
 
 ############################### set parameters -- read from a config file ######################################
 with open(config, 'r') as file:
