52ab940d7aac913a04469b62989d6a79a2f575a4
Kathryn Plant <kplant@lwacalim06.mcs.pvt>
2024-07-09 00:17:49 +0000

    This script now applies the first stage cuts and runs model fits on the events that pass that.

--- processdata1.sh
+++ processdata1.sh
@@ -3,10 +3,20 @@
 
 #Run this script in the "deployment" conda environment
 
+#prepare directories
 mkdir /lustre/kplant/new-dataproducts
-cp /home/kplant/lwa-cosmic-rays/config_preliminary_cuts.yml /lustre/kplant/new-dataproducts/
+cp /home/kplant/lwa-cosmic-rays/config.yml /lustre/kplant/new-dataproducts/
 cd /home/kplant/lwa-cosmic-rays
+
+#Compute summary statistics for all complete events
 ls /lustre/kplant/newdata/*.dat | parallel --jobs 50 python summarize_events.py     /lustre/kplant/new-dataproducts/config_preliminary_cuts.yml
+
+#Make first cuts based on the summaries
+python impulsive_and_antquality_cuts.py /lustre/kplant/new-dataproducts/config.yml
+
+#Do model fits on events that pass those first cuts
+ls /lustre/kplant/new-dataproducts/first_cut_* | parallel python model_fits.py /lustre/kplant/new-dataproducts/config.yml
+
 
 mv /lustre/kplant/newdata /lustre/kplant/$1
 mv /lustre/kplant/new-dataproducts/* /lustre/kplant/$1-dataproducts
